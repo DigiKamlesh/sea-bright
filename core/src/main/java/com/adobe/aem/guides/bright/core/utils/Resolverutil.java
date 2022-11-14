2a4939529e50e092230cfd2940f36a4e0aeefbe6
DESKTOP-OP731TV\santosh <vsk.engg@gmail.com>
2022-11-14 16:24:16 +0530

    on behalf of Anil for events

---
+++ core/src/main/java/com/adobe/aem/guides/bright/core/utils/Resolverutil.java
@@ -0,0 +1,27 @@
+package com.adobe.aem.guides.bright.core.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+
+public class Resolverutil {
+
+
+
+    public  final static String SEABRIGHT__USER = "seabrightuser";
+    /**
+     * @param  resourceResolverFactory factory
+     * @return new resource resolver for Sony service user 
+     * @throws LoginException if problems
+     */
+    public static ResourceResolver newResolver( ResourceResolverFactory resourceResolverFactory ) throws LoginException {
+        final Map<String, Object> paramMap = new HashMap<String, Object>();
+        paramMap.put( ResourceResolverFactory.SUBSERVICE,SEABRIGHT__USER);
+
+        // fetches the admin service resolver using service user.
+        ResourceResolver resolver = resourceResolverFactory.getServiceResourceResolver(paramMap);
+        return resolver;
+    }
+}
